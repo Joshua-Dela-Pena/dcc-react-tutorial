e346ee5925506676d4616f8297b1fcac3de38df8
Joshua-Dela-Pena <richmelldelapena@gmail.com>
2022-01-04 15:41:10 -0600

    Created AddEntryForm component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 174, date: '11-24-2021'}, {weight: 173, date: '11-25-2021'}])
+  const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 174, date: '11-24-2021'}])
+
+  function addNewEntry(entry){
+
+    let tempEntries = [entry, ...entries];
+
+    setEntries(tempEntries);
+  }
 
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm AddNewEntryProperty={addNewEntry} />
     </div>
   );
 }
